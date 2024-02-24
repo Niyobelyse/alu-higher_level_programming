04b7169238456bc53f32efdda0118af9fddad5d0
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2024-02-24 08:10:12 -0800

    final

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# prints all integers of a listi
+# @i iterated all item in the givin list
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print('{:d}'.format(i))
+
+# a = [1, 2, 3, 4]
+# print_list_integer(a)
