3faa1258e71ba493bddb1f0f4a9cdfec5dc6b4ff
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2024-02-24 08:27:06 -0800

    final

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -11,9 +11,6 @@ def replace_in_list(my_list, idx, element):
         my_list[idx] = element
         new_list = my_list
         return new_list
-
-
 # a = [1, 2, 3]
 # replace_in_list(a, 1, 4)
-
 # print(replace_in_list(a, 1, 4))
