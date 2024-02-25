ba8065128b98545f8d9ca4ff643f8e7b9128bc63
Niyonsenga-Belyse <b.niyonseng@alustudent.com>
2024-02-25 02:18:58 -0800

    final

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Prints a matrix of integers
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+        print("")
+
+'''
+matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+print_matrix_integer(matrix)
+'''
